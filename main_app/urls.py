1b822dff9efc5c5d7093fe46d8a4e24206ff7064
n001ce <a1ccormier@gmail.com>
2021-09-03 11:42:42 -0400

    animal index rendering on page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('animals/', views.animals_index, name='animals_index'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,21 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def animals_index(request):
+  return render(request, 'animals/index.html', { 'animals': animals })
+
+class Animal :
+    def __init__(self, type, name, breed, age, owner, description):
+        self.type=type
+        self.name=name
+        self.breed=breed
+        self.age=age
+        self.owner=owner
+        self.description=description
+animals=[
+    Animal('Snake', 'Bali', 'Python', 3, 'Jessica Schmidt', 'Im a slithery snake'),
+    Animal('Dog', 'Ruffus', 'Lab', 5, 'Allan Might', 'Im a good boy'),
+    Animal('Cat', 'Precious', 'American Bobtail', 3, 'Steven Tai', 'Diva'),
+    Animal('Rabbit', 'Hoppy', 'Netherland Dwarf', 3, 'Michelle Olama', 'They are a real jumper'),
+    ]
