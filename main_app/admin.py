31801d8df43bf65a4e2cb1f2d5ece59ebc36e4fc
n001ce <a1ccormier@gmail.com>
2021-09-03 12:46:43 -0400

    feeding model stubbed up

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Animal
+from .models import Animal, Feeding
 
 # Register your models here
 admin.site.register(Animal)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('D', 'Dinner')
+)
+
 class Animal(models.Model):
     type = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -14,3 +19,15 @@ class Animal(models.Model):
     
     def get_absolute_url(self):
         return reverse('animals_detail', kwargs={'animal_id':self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices = MEALS,
+        default=MEALS[0][0]
+        )
+    food = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
