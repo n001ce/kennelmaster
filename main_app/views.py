f50ede2f3e7c3bb88af8727786589ac145965666
n001ce <a1ccormier@gmail.com>
2021-09-03 11:58:22 -0400

    animals from db rendering

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Animal
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
 def animals_index(request):
-  return render(request, 'animals/index.html', { 'animals': animals })
+    animals = Animal.objects.all()
+    return render(request, 'animals/index.html', { 'animals': animals })
 
-class Animal :
-    def __init__(self, type, name, breed, age, owner, description):
-        self.type=type
-        self.name=name
-        self.breed=breed
-        self.age=age
-        self.owner=owner
-        self.description=description
-animals=[
-    Animal('Snake', 'Bali', 'Python', 3, 'Jessica Schmidt', 'Im a slithery snake'),
-    Animal('Dog', 'Ruffus', 'Lab', 5, 'Allan Might', 'Im a good boy'),
-    Animal('Cat', 'Precious', 'American Bobtail', 3, 'Steven Tai', 'Diva'),
-    Animal('Rabbit', 'Hoppy', 'Netherland Dwarf', 3, 'Michelle Olama', 'They are a real jumper'),
-    ]
