90d31dd2dc48c237fc2f17edf3e1fa0564ed0873
n001ce <a1ccormier@gmail.com>
2021-09-03 14:19:27 -0400

    POST new feedings

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Animal
 from .forms import FeedingForm
@@ -17,6 +17,14 @@ def animals_detail(request, animal_id):
   animal = Animal.objects.get(id=animal_id)
   feeding_form = FeedingForm()
   return render(request, 'animals/detail.html', { 'animal': animal, 'feeding_form':feeding_form })
+
+def add_feeding(request, animal_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.animal_id = animal_id
+        new_feeding.save()
+    return redirect('animals_detail', animal_id=animal_id)
 
 class AnimalCreate(CreateView):
   model = Animal
