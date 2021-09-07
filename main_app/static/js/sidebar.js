3047ee815c5a584fdf6a0dde55fdf408b2ecd9c8
n001ce <a1ccormier@gmail.com>
2021-09-07 12:15:07 -0400

    updated styling

--- main_app/static/js/sidebar.js
+++ main_app/static/js/sidebar.js
@@ -1,7 +1,13 @@
 let btn = document.querySelector("#btn")
 let sidebar = document.querySelector(".sidebar")
+let main = document.querySelector(".main")
+let header = document.querySelector(".header")
+let footer = document.querySelector(".footer")
 
 
 btn.onclick = function(){
   sidebar.classList.toggle("active")
+  main.classList.toggle("active")
+  header.classList.toggle("active")
+  footer.classList.toggle("active")
 }
